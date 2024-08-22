68f488ae08652efa6dca304e5d43b28e34ca3f24
vshveta08 <vshveta08@gmail.com>
2024-08-22 21:46:18 +0530

    update

--- blog-app-backend/app.js
+++ blog-app-backend/app.js
@@ -7,17 +7,11 @@ const routes = require("./routes/index");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
     credentials: true,
   })
 );
 
 app.use(express.json());
-
-
-app.get("/", (req, res) => {
-  res.send("hii");
-});
 
 // set router
 app.use("/api", routes);
