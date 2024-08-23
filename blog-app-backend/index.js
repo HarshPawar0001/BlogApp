4cd38b7ad2c98d49edfb96883de08b6540c55246
vshveta08 <vshveta08@gmail.com>
2024-08-23 08:13:03 +0530

    update

--- blog-app-backend/index.js
+++ blog-app-backend/index.js
@@ -4,17 +4,18 @@ const cors = require("cors");
 const { PORT } = require("./config/index");
 const connectDB = require("./config/connectDb");
 const routes = require("./routes/index");
-const allowedOrigins = ['http://localhost:5173', 'https://blog-web-application-psi.vercel.app/'];
+const allowedOrigins = ['http://localhost:5173', 'https://blog-web-application-psi.vercel.app'];
 
 const corsOrigin = {
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true,
+  credentials: true, // Allow credentials such as cookies
 };
 app.use(cors(corsOrigin));
 // app.use(cors());
